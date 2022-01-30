33e838e5c5cac44ed8cd5a3e873e4d6838e02f67
nakashima <nakashima@hab-co.jp>
2022-01-30 17:36:29 +0900

    fontfamily 追加方法コメントアウト

--- wp-content/themes/original/functions.php
+++ wp-content/themes/original/functions.php
@@ -12,10 +12,8 @@ function load_assets()
 {
   // キャッシュ対策のためデプロイ前に$versionの数値を変更する。
   $version = '1.0.0';
-  /* google fonts Noto+Sans+JP  */
+  /* (例)google fonts Noto+Sans+JP font-weightはデザインのweightを選択する */
   // wp_enqueue_style('Noto+Sans+JP', 'https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@400;700&display=swap', array(), $version, false);
-  /* google fonts Roboto+Condensed  */
-  // wp_enqueue_style('Roboto+Condensed', 'https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap', array(), $version, false);
   /* common */
   wp_enqueue_style('my-style', get_template_directory_uri() . '/style.css', array(), $version, false);
   wp_enqueue_script('my-js', get_template_directory_uri() . '/js/common.js', array('jquery'), $version, true);
